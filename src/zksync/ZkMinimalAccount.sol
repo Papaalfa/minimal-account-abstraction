cf11389a1a161dd98567acf8b6ee19f5cb4cac94
Sergey Zimin <s@zimin.me>
2026-03-28 00:37:13 +0100

    Downgraded account-abstraction, started validateTransaction function

---
+++ src/zksync/ZkMinimalAccount.sol
@@ -0,0 +1,54 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {IAccount} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
+import {Transaction} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
+import {SystemContractsCaller} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/SystemContractsCaller.sol";
+import {NONCE_HOLDER_SYSTEM_CONTRACT} from "lib/foundry-era-contracts/src/system-contracts/contracts/Constants.sol";
+import {INonceHolder} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/INonceHolder.sol";
+
+contract ZkMinimalAccount is IAccount {
+    /*//////////////////////////////////////////////////////////////
+                       EXTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    function validateTransaction(
+        bytes32 _txHash,
+        bytes32 _suggestedSignedHash,
+        Transaction memory _transaction
+    ) external payable returns (bytes4 magic) {
+        // call nonce holder
+        // increment nonce
+        // call(x, y, z) --> system contract call
+        SystemContractsCaller.systemCallWithPropagatedRevert(
+            uint32(gasleft()),
+            address(NONCE_HOLDER_SYSTEM_CONTRACT),
+            0,
+            abi.encodeCall(INonceHolder.incrementMinNonceIfEquals, (_transaction.nonce))
+        );
+    }
+
+    function executeTransaction(
+        bytes32 _txHash,
+        bytes32 _suggestedSignedHash,
+        Transaction memory _transaction
+    ) external payable {}
+
+    function executeTransactionFromOutside(Transaction memory _transaction) external payable {}
+
+    function payForTransaction(
+        bytes32 _txHash,
+        bytes32 _suggestedSignedHash,
+        Transaction memory _transaction
+    ) external payable {}
+
+    function prepareForPaymaster(
+        bytes32 _txHash,
+        bytes32 _possibleSignedHash,
+        Transaction memory _transaction
+    ) external payable {}
+
+    /*//////////////////////////////////////////////////////////////
+                       INTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+}
